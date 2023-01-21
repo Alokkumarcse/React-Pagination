20bf3aa486e880bdfbfd7b227667fcdbcff9cc38
Alok Kumar <alokk13799@gmail.com>
2023-01-22 00:02:53 +0530

    Navbar added

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,23 @@ import { useEffect, useState } from 'react';
 
 import styles from '../styles/App.module.css';
 import ItemCard from './ItemCard';
+import Navbar from './Navbar';
 
 function App() {
   // useState hooks to update our products list
   const [products, setProducts] = useState([]);
   /** specific page number render */
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
   
   /** fetch data form api */
   const fetchProducts = async () => {
-    const res = await fetch('https://dummyjson.com/products/?limit=100');
+    const res = await fetch(`https://dummyjson.com/products/?limit=10&&skip=${page*10-10}`);
     const data = await res.json();
     /** set products list using setProducts(argument) method */ 
     if(data && data.products){
       setProducts(data.products);
+      setTotal(data.total);
     }
   }
 
@@ -40,18 +43,33 @@ function App() {
 
   /** Function for handle showNextPage() event from pagination */
   const  showNextPage = () => {
-    page < (products.length/10) && 
+    page < (total/10) && 
     setPage(page + 1);
     return;
   }
 
   return (
     <div className={styles.container}>
+      <Navbar className={styles.sticky}/>
       {/* Product list section */}
       <section className={styles.products}>
-        {
+        {/* {
           products.length > 0 &&
           products.slice(page*10-10, page*10 ).map((item, index) => {
+            const {thumbnail, title, description, price} = item;
+            return (
+            <ItemCard 
+              key={`${index}ade`} 
+              thumbnail={thumbnail} 
+              title={title} 
+              description={description}
+              price={price}
+            />)
+          })
+        } */}
+        {
+          products.length > 0 &&
+          products.map((item, index) => {
             const {thumbnail, title, description, price} = item;
             return (
             <ItemCard 
@@ -72,18 +90,20 @@ function App() {
           page === 1
           ? null
           : <span 
-              className={styles.btn}
+              className={`${styles.btn} ${styles.pagination__span}`}
               onClick={() => showPrevPage()} 
             >◀</span> 
         }
         {
           // create new empty array of size and spread so each element assign undefined by default.
-          [...Array(products.length/10)].map((item, index) => {
+          // [...Array(products.length/10)].map((item, index) => {
+            [...Array(total/10)].map((item, index) => {
             return (
               <span 
                 key={`${index}index`}
                 onClick={() => selectedPageHandler(index+1)}
-                className={ page === index + 1 ? `${styles.active}`: ""}
+                // we can write condition inside class attribute value as well
+                className={ page === index + 1 ? `${styles.active} ${styles.pagination__span}`: `${styles.pagination__span}`}
               >{index + 1} </span>
             )
           })
@@ -92,7 +112,7 @@ function App() {
         {
           page <10
           ? <span 
-              className={styles.btn}
+              className={`${styles.btn} ${styles.pagination__span}`}
               onClick={() => {showNextPage()}}
             >▶</span>   
           : null
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from '../styles/Navbar.module.css';
+function Navbar() {
+  return (
+   <nav className={styles.nav}>
+      <div className={styles.navbar}>
+         pagination
+      </div>
+   </nav>
+   
+  )
+}
+
+export default  Navbar;
