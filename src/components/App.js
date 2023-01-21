be35b3a9930c5ec3cd25507a3feeba285b3f3f1f
Alok Kumar <alokk13799@gmail.com>
2023-01-21 22:25:03 +0530

    page handler added

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import ItemCard from './ItemCard';
 function App() {
   // useState hooks to update our products list
   const [products, setProducts] = useState([]);
+  /** specific page number render */
+  const [page, setPage] = useState(1);
   
   /** fetch data form api */
   const fetchProducts = async () => {
@@ -20,14 +22,36 @@ function App() {
   /** useEffect hooks to help call the fetch function and re-render app when dependencies[] changed */
   useEffect(() => {
     fetchProducts();
-  }, []);
+    setPage(page);
+  }, [page]);
+
+  /** Function for handle when page has been selected */
+  const selectedPageHandler = (page) => {
+    setPage(page);
+    return;
+  }
+
+  /** Function for handle showPrevPage() event from pagination */
+  const showPrevPage = () => {
+    page > 1 && 
+    setPage(page -1);
+    return;
+  }
+
+  /** Function for handle showNextPage() event from pagination */
+  const  showNextPage = () => {
+    page < (products.length/10) && 
+    setPage(page + 1);
+    return;
+  }
 
   return (
     <div className={styles.container}>
       {/* Product list section */}
       <section className={styles.products}>
         {
-          products.map((item, index) => {
+          products.length > 0 &&
+          products.slice(page*10-10, page*10 ).map((item, index) => {
             const {thumbnail, title, description, price} = item;
             return (
             <ItemCard 
@@ -41,15 +65,38 @@ function App() {
         }
       </section>
 
-      {/* pagination  */}
+      {/* pagination logic */}
       <section className={styles.pagination}>
-        <span >◀</span> 
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>   
-        <span>▶</span>    
-      </section>
-      
+        {/* condition to show and hide prev arrow */}
+        {
+          page === 1
+          ? null
+          : <span 
+              className={styles.btn}
+              onClick={() => showPrevPage()} 
+            >◀</span> 
+        }
+        {
+          // create new empty array of size and spread so each element is assign undefined
+          [...Array(products.length/10)].map((item, index) => {
+            return (
+              <span 
+                key={`${index}index`}
+                onClick={() => selectedPageHandler(index+1)}
+              >{index + 1} </span>
+            )
+          })
+        }
+        {/* condition to show and hide next arrow */}
+        {
+          page <10
+          ? <span 
+              className={styles.btn}
+              onClick={() => {showNextPage()}}
+            >▶</span>   
+          : null
+        } 
+      </section> 
     </div>
   );
 }
